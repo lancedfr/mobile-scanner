8a63a4894db81d3bf6a4fb196b275d9988c92ef5
tohhierallie <tohhierallie@gmail.com>
2014-08-11 15:22:05 +0200

    Scanner - issue with rotation resolved, zxing integrated added in place of dependance on barcode scanner

--- Scanner/app/src/main/java/tohhier/scanner/ScannerActivity.java
+++ Scanner/app/src/main/java/tohhier/scanner/ScannerActivity.java
@@ -9,9 +9,15 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+//import tohhier.scanner.IntentIntegrator;
+//import tohhier.scanner.IntentResult;
 
 public class ScannerActivity extends Activity {
 
@@ -19,13 +25,40 @@ public class ScannerActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scanner);
+        setContentView(R.layout.activity_scanner); // this works without the scanner app
+      //  IntentIntegrator.setResultDisplayDuration(getIntent(),50000);
         IntentIntegrator.initiateScan(this);
 
+       // IntentIntegrator integrator = new IntentIntegrator(this);
+       // integrator.initiateScan();
+       /* IntentIntegrator integrator = new IntentIntegrator(ScannerActivity.this);
+        integrator.addExtra("SCAN_WIDTH", 800);
+        integrator.addExtra("SCAN_HEIGHT", 300);
+        integrator.addExtra("RESULT_DISPLAY_DURATION_MS", 3000L);
+        integrator.addExtra("PROMPT_MESSAGE", "Scan Product Barcode");
 
+        integrator.initiateScan(IntentIntegrator.PRODUCT_CODE_TYPES);*/
 
 
     }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+           String barcode;
+           String type;
 
+           barcode = scanResult.getContents();
+           type = scanResult.getFormatName();
+
+            EditText barcodeText = (EditText)findViewById(R.id.barcodeText);
+            EditText typeText = (EditText)findViewById(R.id.typeText);
+
+            barcodeText.setText(barcode);
+            typeText.setText(type);
+
+
+        }
+
+    }
 
 }
