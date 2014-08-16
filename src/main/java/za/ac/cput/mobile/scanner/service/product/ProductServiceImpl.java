b7928de920fe2d9bda147faeff47942cf78fceb0
lancedfr <lancedfr@outlook.com>
2014-08-16 23:08:38 +0200

    Made Services layer

---
+++ src/main/java/za/ac/cput/mobile/scanner/service/product/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package za.ac.cput.mobile.scanner.service.product;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import za.ac.cput.mobile.scanner.repository.dao.ProductDao;
+import za.ac.cput.mobile.scanner.repository.model.Product;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+  @Autowired
+  private ProductDao productDao;
+
+  @Override
+  public void addProduct(Product product) {
+    productDao.addProduct(product);
+  }
+
+  @Override
+  public void updateProduct(Product product) {
+    productDao.updateProduct(product);
+  }
+
+  @Override
+  public Product getProduct(Integer id) {
+    return productDao.getProduct(id);
+  }
+
+  @Override
+  public void deleteProduct(Integer id) {
+    productDao.deleteProduct(id);
+  }
+
+  @Override
+  public List<Product> getProducts() {
+    return productDao.getProducts();
+  }
+
+}
