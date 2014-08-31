de579e7e50baab84e8f08efe1f794dfbb7fd5ef7
lancedfr <lancedfr@outlook.com>
2014-09-01 00:08:37 +0200

    Improved test

--- src/test/java/za/ac/cput/mobile/scanner/repository/ProductDaoIT.java
+++ src/test/java/za/ac/cput/mobile/scanner/repository/ProductDaoIT.java
@@ -1,9 +1,10 @@
 package za.ac.cput.mobile.scanner.repository;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +17,44 @@ import za.ac.cput.mobile.scanner.repository.model.Product;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:repository-context-test.xml")
 public class ProductDaoIT {
-  
+
   @Autowired
   private ProductDao productDao;
+  private Product product;
 
-  @Test
-  public void test() {
-    Product product = new Product();
+  @Before
+  public void before() {
+    product = new Product();
     product.setBarcode("1234123123");
     product.setName("Lance");
+  }
+
+  @Test
+  public void testGetProduct() throws Exception {
     productDao.addProduct(product);
+    Product getProduct = productDao.getProduct(1);
+    assertEquals("1234123123", getProduct.getBarcode());
+  }
+
+  @Test
+  public void testUpdateProduct() throws Exception {
+    Product getProduct1 = productDao.getProduct(1);
+    getProduct1.setName("Other");
+    productDao.updateProduct(getProduct1);
+    Product getProduct2 = productDao.getProduct(1);
+    assertEquals("Other", getProduct2.getName());
+  }
+
+  @Test
+  public void testAddProduct() {
+    productDao.addProduct(product);
+    List<Product> products = productDao.getProducts();
+    assertEquals(2, products.size());
+  }
+
+  @Test
+  public void testDeleteProduct() throws Exception {
+    productDao.deleteProduct(1);
     List<Product> products = productDao.getProducts();
     assertEquals(1, products.size());
   }
