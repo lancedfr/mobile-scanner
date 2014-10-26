2672438645f7d87691ad750de65984cc0323123e
tohhierallie <tohhierallie@gmail.com>
2014-10-26 17:04:31 +0200

    POST add new product works

--- Scanner/app/src/main/java/tohhier/scanner/Fragment2.java
+++ Scanner/app/src/main/java/tohhier/scanner/Fragment2.java
@@ -1,10 +1,12 @@
 package tohhier.scanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Fragment2 extends Fragment {
@@ -34,8 +36,19 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_fragment2, container, false);
+        Button b = (Button)rootView.findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getActivity(), NewProductScannerActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+        return rootView;
     }
 
 
