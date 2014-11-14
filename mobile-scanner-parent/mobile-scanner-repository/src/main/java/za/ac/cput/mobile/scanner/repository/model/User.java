6ac7f1d1f5dc0d66561bc882ef3ce6b94c022505
nashnaidoo <nashen.naidoo@gmail.com>
2014-11-14 17:11:00 +0200

    Adding User

---
+++ mobile-scanner-parent/mobile-scanner-repository/src/main/java/za/ac/cput/mobile/scanner/repository/model/User.java
@@ -0,0 +1,78 @@
+package za.ac.cput.mobile.scanner.repository.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * The Class User.
+ */
+@Entity
+@Table(name = "user")
+public class User {
+
+	/** The id. */
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String password;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Integer age;
+	
+	private String gender;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
