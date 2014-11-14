eac9f722b3133b4cd5995482193f184be4d6cb91
nashnaidoo <nashen.naidoo@gmail.com>
2014-11-14 17:54:47 +0200

    Fix

--- mobile-scanner-parent/mobile-scanner-service/src/main/java/za/ac/cput/mobile/scanner/service/product/UserServiceImpl.java
+++ mobile-scanner-parent/mobile-scanner-service/src/main/java/za/ac/cput/mobile/scanner/service/product/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package za.ac.cput.mobile.scanner.service.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import za.ac.cput.mobile.scanner.repository.dao.UserDao;
 import za.ac.cput.mobile.scanner.repository.model.User;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
